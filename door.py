e385b02cda659f54e6d3cf501f98d1f89a65fdda
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-06 00:04:03 -0800

    Update door.py

--- door.py
+++ door.py
@@ -6,3 +6,9 @@ this program uses python flask to host the sign on my door
 
 from flask import Flask, render_template,
 
+app = Flask(__name__, template_folder='html/templates/')
+
+
+
+if __name__ == '__main__' :
+    app.run(host='::', port=80)

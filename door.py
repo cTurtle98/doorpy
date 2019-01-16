1527566e9ac0ed7594bf412a8b9803c750cb1d9c
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-15 19:34:30 -0800

    Update door.py

--- door.py
+++ door.py
@@ -4,11 +4,27 @@ pydoor python program
 this program uses python flask to host the sign on my door
 """
 
-from flask import Flask, render_template,
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='html/templates/')
 
-
+@app.route('/')
+def index() :
+    cardnum = request.args.get('card')
+    if cardnum = "None":
+        return render_template('pleasescan.html',)
+    
+    elif cardsdict[cardnum]['active'] = 'A':
+        cardsdict[cardnum]['active'] = 'B'
+        return render_template(cardsdict[cardnum]['B'],)
+    
+    elif cardsdict[cardnum][active] = 'B':
+        cardsdict[cardnum][active] = 'A'
+        return render_template(cardsdict[cardnum]['A'],)
+    
+    else:
+        return render_template('cardnotfound.html',)
+    
 
 if __name__ == '__main__' :
     app.run(host='::', port=80)

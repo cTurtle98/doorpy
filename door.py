b43162f5e7b0fce23f3907bfea634c4b7e9e06e9
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-22 15:47:03 -0800

    Update door.py
    fix sum shit

--- door.py
+++ door.py
@@ -7,8 +7,6 @@ this program uses python flask to host the sign on my door
 
 from flask import Flask, render_template, request, abort
 import json
-from os import listdir
-from os.path import isfile, join
 
 #enable personal debug messages
 DEBUG = True
@@ -32,6 +30,7 @@ except:
     }
 ## END BAD CODE BLOCK
 
+# load messages json into dictionary for runtime use
 try:
     f = open("messages.json")
     messagesdict = json.load(f)
@@ -40,22 +39,21 @@ except:
     messagesdict = {}
     print("ERROR! Could not load messages dictionary!")
 
+# setup the flask enviroment
 app = Flask(__name__, template_folder='html/templates/')
 
 @app.route('/')
 def index() :
+'''this is the main route that the kiosk pi will be looking at'''
+    
+    #retrieve cardnum from html form
     cardnum = request.args.get('card')
     
-    if DEBUG:
-        print("DEBUG: cardnum = ", cardnum)
-        print("DEBUG: cardnum type = ", type(cardnum))
-        
-    
+    #if you havent scanned a card yet display a page telling you to
     if cardnum == None:
-        if DEBUG:
-            print("DEBUG: messagesdict default", messagesdict["default"]["template"])
         return render_template(messagesdict["default"]["template"], **messagesdict["default"])
     
+    #process scanning a card and retrieving its message
     try:
         if cardsdict[cardnum]['active'] == 'A':
             cardsdict[cardnum]['active'] = 'B'
@@ -68,15 +66,9 @@ def index() :
     
 @app.route('/edit')
 def edit() :
-    mypath = "html/templates/"
-    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    files.remove("base.html")
-    files.remove('template.html')
-    
-    if DEBUG:
-        print(files)
-        
-    return str(files)
+''' this is a route for registering new cards and messages, must be accessed remotely'''
+
+    return abort(404)
 
 if __name__ == '__main__' :
     # WARNING FLASK IS IN DEBUG MODE DISABLE FOR PRODUCTION SERVER

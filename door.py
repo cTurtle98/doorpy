98eb9b540c3d91103c3d09c2dacd933bf3b6ee58
Robert <robertgarrett404@gmail.com>
2019-01-22 06:54:41 -0700

    Created sample JSON, SignOnDoor │robert@TheMatrix:~/Workspaces/python-workspace/doorpy/.git$ ls class for scope resolution. Needs Templates to be fleshed out next

--- door.py
+++ door.py
@@ -8,88 +8,70 @@ from flask import Flask, render_template, request
 import json
 from os import listdir
 from os.path import isfile, join
+class SignOnDoor:
+    """
+    python has some weird things goin on with scope.
+    I created a class to force the use of SignOnDoor.mostRecentCard
+    rather than some index() local variable that gets created in-place
+    """
 
-#enable personal debug messages
-DEBUG = True
+    #enable personal debug messages
+    DEBUG = True
 
-try:
-    with open("cards.json") as f:
-        cardsdict = json.load(f)
-except:
-    #MINIMAL DICTS FOR TESTING
-    cardsdict = {
-        #card number 12345
-        "12345" : {
-            #primary message
-            "A" : "home",
-            #secondary message
-            "B" : "away",
-            #was last seen message primary or secondary
-            "active" : "A"
-        },
-    }
-    
-try:
-    with open("messages.json") as f:
-        messagesdict = json.load(f)
-except:
-    #MINIMAL DICTS FOR TESTING
-    messagesdict = {
-        # home message
-        "home" : {
-            # jinja template to use to render message
-            "template" : "basicmessage.html",
-            # message type for css
-            "messageType" : "home",
-            # title for message
-            "title" : "Home!",
-            # status for message
-            "status" : "Enter at your own risk!"
-        },
-        "away" : {
-            "template" : "calendar.html",
-            "messageType" : "away",
-            "title" : "Away!",
-            "status" : "see calendar"
-        },
-    }
-
-app = Flask(__name__, template_folder='html/templates/')
-
-@app.route('/')
-def index() :
-    cardnum = request.args.get('card')
-    
-    if DEBUG:
-        print("DEBUG: cardnum = ", cardnum)
-        print("DEBUG: cardnum type = ", type(cardnum))
-        
-    
-    if str(cardnum) == str("None"):
-        return render_template('pleasescan.html',)
-    
+    app = Flask(__name__, template_folder='html/templates/')
+    mostRecentCard = {}  # tracks which card was most recently handed to the Raspi Sign
+    extraMode = False # trakcs if template should be augmented with ModeB, or standard ModeA (+/- calendar)
+    cardsdict = {}
     try:
-        if cardsdict[cardnum]['active'] == 'A':
-            cardsdict[cardnum]['active'] = 'B'
-            return render_template(messagesdict[cardsdict[cardnum]['B']]['template'], **messagesdict[cardsdict[cardnum]['B']])    
-        elif cardsdict[cardnum][active] == 'B':
-            cardsdict[cardnum][active] = 'A'
-            return render_template(cardsdict[cardnum]['A'],)
+        # load card pages from cards.json
+        f = open("cards.json")
+        cardsdict = json.load(f)
+        print("DEBUG: cardsdict contents: ", cardsdict)
     except:
-        return render_template('cardnotfound.html',)
-    
-@app.route('/edit')
-def edit() :
-    mypath = "html/templates/"
-    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    files.remove("base.html")
-    files.remove('template.html')
-    
-    if DEBUG:
-        print(files)
+        print("an error has occurred!")
+    @app.route('/')
+    def index():
+        #TODO: Unset this string assignment. This was just for testing without the card reader
+        #TODO: ensure form data is aquired correctly
+        cardnum = "card1" # request.args.get("card") # request card number from Raspi Sign
         
-    return str(files)
+        if SignOnDoor.DEBUG:
+            print("DEBUG: cardnum = ", cardnum)
+            print("DEBUG: cardnum type = ", type(cardnum))
+            print("DEBUG: app type = ", type(SignOnDoor.app))
+        try:  # surround with try-except to handle card-not-found errors
+        # if there was no mostRecentCard (server has just started), display a prompt to swipe first one!
+            if(SignOnDoor.mostRecentCard == {}):  # <--- This here is why there is a class, now
+                SignOnDoor.mostRecentCard = SignOnDoor.cardsdict[cardnum] # assign first setting
+                return render_template('pleasescan.html')
+            # if card has been swiped 2x in a row, toggle the extra mode
+            if SignOnDoor.cardsdict[cardnum] == SignOnDoor.mostRecentCard:
+                SignOnDoor.extraMode = not SignOnDoor.extraMode
+            else: # if its a different card, then reset to False
+                SignOnDoor.extraMode = False
+            # use the template associated with this card,
+            # pass in its dictionary of properties 
+            # (keys are used as Jinja2 variable names, and the values are of course values)
+            # add withCalendar keyword argument
+            print("DEBUG: Rendering card with properties: ", SignOnDoor.cardsdict[cardnum])
+            return render_template(SignOnDoor.cardsdict[cardnum]["template"], **SignOnDoor.cardsdict[cardnum], withCalendar=SignOnDoor.extraMode)
+            # passing in parameters this way allows for unlimited length lists of parameters (or until RAM caps out, anyway)        
+    
+        except KeyError: # handle invalid card values by showing error
+            return render_template('basicmessage.html', title="Invalid Card", status="Card is Invalid")
+        
+    @app.route('/edit')
+    def edit() :
+        mypath = "html/templates/"
+        files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        files.remove("base.html")
+        files.remove('template.html')
+        
+        if DEBUG:
+            print(files)
+            
+        return str(files)
 
 if __name__ == '__main__' :
     # WARNING FLASK IS IN DEBUG MODE DISABLE FOR PRODUCTION SERVER
-    app.run(debug=True, host='::', port=80)
+    SignOnDoor.app.run(debug=True, host='::', port=2580)

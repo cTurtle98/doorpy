40f6d94920c7c14a4b1c818aa3af7aa5e56b5bf6
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-23 02:10:16 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,30 @@
 #Ciaran Farley
 # doorpy setup script
 # commands to modify a fresh install of raspbian for use with doorpy
+
+sudo apt remove --purge -y \
+bluej \
+geany \
+greenfoot \
+mathematica \
+minecraft-pi \
+libreoffice* \
+vnc \
+lxde \
+lxpanel \
+lxappearance \
+lxde-common \
+lxde-core \
+lxde-icon-theme \
+lxinput \
+lxinput \
+lxkeymap \
+lxmenu-data \
+lxappearance-obconf \
+lxpanel-data \
+lxrandr \
+lxsession \
+lxtask \
+lxterminal 
+
+sudo apt autoremove --purge

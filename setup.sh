0cca742469561e3dee5955c584e4875fccea4b93
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-23 12:27:59 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@
 # doorpy setup script
 # commands to modify a fresh install of raspbian Lite for use with doorpy
 
+echo "UPDATING DEFAULT INSTALLED PACKAGES"
 sudo apt-get update
 sudo apt-get -y upgrade
 
+echo "INSTALLING MINIMUM GUI FOR MESSAGE BOARD"
 sudo apt-get install -y --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 
+echo "INSTALLING CHROMIUM FOR MESSAGE BOARD"
 sudo apt-get install -y --no-install-recommends chromium-browser
 
+echo "CHANGING OPENBOX CONFIG FOR X11 SETTINGS WE NEED"
 echo '''
 # Disable any form of screen saver / screen blanking / power management
 xset s off
@@ -25,10 +29,13 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/
 chromium-browser --disable-infobars --kiosk 'http://localhost:8080'
 ''' > /etc/xdg/openbox/autostart
 
+echo "ENABLING AUTOMATIC GUI MODE"
 echo '''
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor
 ''' >> $HOME/.bash_profile
 
+echo "ENABLING AUTOMATIC PYTHON SERVER START"
 echo '''
+git pull doorpy &&\
 python3 doorpy/door.py &
 ''' >> $HOME/.bash_profile

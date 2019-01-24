0e6b05294fe7f43d16e13b0f39f12e4ad58d3c71
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-23 23:13:23 -0800

    Create reload.sh

---
+++ configs/reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Ciaran Farley
+#doorpy
+#https://github.com/cTurtle98/doorpy
+
+#kill flask, git pull, start flask
+
+kill $(ps a | grep "python3 door.py" | head -1 | cut -f 3 -d " ")
+
+cd /home/pi/doorpy
+git pull
+python3 door.py &

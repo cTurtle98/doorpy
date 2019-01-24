54605fd7edf712498baef96c38f4eea881872442
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-23 19:44:15 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,33 +26,11 @@ pip install Flask
 echo -*20
 
 echo "CHANGING OPENBOX CONFIG FOR X11 SETTINGS WE NEED"
-echo '''
-# Disable any form of screen saver / screen blanking / power management
-xset s off
-xset s noblank
-xset -dpms
-
-# Allow quitting the X server with CTRL-ATL-Backspace
-setxkbmap -option terminate:ctrl_alt_bksp
-
-# Start Chromium in kiosk mode
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-chromium-browser --disable-infobars --kiosk 'http://localhost:8080'
-''' > /etc/xdg/openbox/autostart
+cat configs/openbox-config > /etc/xdg/openbox/autostart
 
 echo -*20
 
-echo "ENABLING AUTOMATIC GUI MODE"
+echo "adding startup script to .bash_profile"
 echo '''
-[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor
-''' >> $HOME/.bash_profile
-
-echo -*20
-
-echo "ENABLING AUTOMATIC PYTHON SERVER START"
-echo '''
-cd doorpy &&\
-git pull &&\
-python3 door.py &
+source $HOME/doorpy/configs/startup.sh
 ''' >> $HOME/.bash_profile

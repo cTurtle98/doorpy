5dd868ccde1da55b23d33a7d521c0ccfb2bedd39
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-23 22:21:42 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,45 +3,47 @@
 # doorpy setup script
 # commands to modify a fresh install of raspbian Lite for use with doorpy
 
+sep(){
+  printf '#'%.0s {1..98} ; echo
+}
+
 echo "UPDATING DEFAULT INSTALLED PACKAGES"
 sudo apt-get update
 sudo apt-get -y upgrade
 
-printf '#'%.0s {1..98} ; echo
+sep()
 
 echo "INSTALLING MINIMUM GUI FOR MESSAGE BOARD"
 sudo apt-get install -y --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 
-printf '#'%.0s {1..98} ; echo
+
+sep()
 
 echo "INSTALLING CHROMIUM FOR MESSAGE BOARD"
 sudo apt-get install -y --no-install-recommends chromium-browser
 
-printf '#'%.0s {1..98} ; echo
+sep()
 
 echo "INSTALLING PYTHON DEPENDENCIES"
 sudo apt-get install -y python3 python3-pip
 pip3 install Flask
 
-printf '#'%.0s {1..98} ; echo
+sep()
 
 echo "CHANGING OPENBOX CONFIG FOR X11 SETTINGS WE NEED"
 cat configs/openbox-config | sudo tee /etc/xdg/openbox/autostart > /dev/null
 
-printf '#'%.0s {1..98} ; echo
+sep()
 
 echo "adding startup script to .bash_profile"
 cat $HOME/doorpy/configs/startup.sh > $HOME/.bash_profile
 
-printf '#'%.0s {1..98} ; echo
+sep()
 
-#ROTATING MONITOR
+echo "MONITOR SETTINGS CHANGE"
 
-if grep -Fxq "display_rotate=3" /boot/config.txt
-then
-  echo "display already rotated"
-else
-  echo "display_rotate=3" | sudo tee /boot/config.txt > /dev/null
-fi
+cat $HOME/doorpy/configs/bootconfig | sudo tee /boot/config.txt > /dev/null
+
+sep()
 
 echo "SETUP COMPLETE PLEASE REBOOT"

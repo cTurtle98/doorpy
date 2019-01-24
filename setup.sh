16b9b8ff3e02740568bf7bc7ee8aed0e4a152b94
Ciaran Farley <cturtle98@98seaturtles.net>
2019-01-23 21:00:05 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,31 +7,32 @@ echo "UPDATING DEFAULT INSTALLED PACKAGES"
 sudo apt-get update
 sudo apt-get -y upgrade
 
-echo -*20
+printf '#'%.0s {1..98} ; echo
 
 echo "INSTALLING MINIMUM GUI FOR MESSAGE BOARD"
 sudo apt-get install -y --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
 
-echo -*20
+printf '#'%.0s {1..98} ; echo
 
 echo "INSTALLING CHROMIUM FOR MESSAGE BOARD"
 sudo apt-get install -y --no-install-recommends chromium-browser
 
-echo -*20
+printf '#'%.0s {1..98} ; echo
 
 echo "INSTALLING PYTHON DEPENDENCIES"
 sudo apt-get install -y python3 python-pip
 pip install Flask
 
-echo -*20
+printf '#'%.0s {1..98} ; echo
 
 echo "CHANGING OPENBOX CONFIG FOR X11 SETTINGS WE NEED"
 cat configs/openbox-config | sudo tee /etc/xdg/openbox/autostart > /dev/null
 
-echo -*20
+printf '#'%.0s {1..98} ; echo
 
 echo "adding startup script to .bash_profile"
 cat $HOME/doorpy/configs/startup.sh > $HOME/.bash_profile
 
-echo -*20
+printf '#'%.0s {1..98} ; echo
+
 echo "SETUP COMPLETE PLEASE REBOOT"

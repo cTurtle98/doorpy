521fcbddaafa75a698a54d8dfa6c8b4fbb82a8c1
Ciaran Farley <ciaran@cturtle98.com>
2020-03-28 14:45:19 -0700

    Create setup.py
    process of converting program to python module

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+
+webpy module setup.py
+By Ciaran Farley
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+

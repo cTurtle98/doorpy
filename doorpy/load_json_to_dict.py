b94042b918cba8731ec20e83634a8c499f08e408
Ciaran Farley <ciaran@cturtle98.com>
2020-03-30 03:11:22 -0700

    Update load_json_to_dict.py

--- doorpy/load_json_to_dict.py
+++ doorpy/load_json_to_dict.py
@@ -29,39 +29,40 @@ from doorpy import cardsdict
 from doorpy import messagesdict
 from doorpy import configsdict
 
-
-try:
-  f = open("configs.json")
-  configsdict = json.load(f)
-  f.close
-except:
-  print("ERROR! Could not load configs dictionary!")
-# load cards json into dictionary for runtime use
-try:
-  f = open("json/cards.json")
-  cardsdict = json.load(f)
-  f.close
-except:
+def load_json_to_dict():
+  # load configs json into dictionary for runtime use
   try:
-    print("custom cards not found, attempting to load default-cards")
-    f = open("json/default-cards.json")
+    f = open("configs.json")
+    configsdict = json.load(f)
+    f.close
+  except:
+    print("ERROR! Could not load configs dictionary!")
+  # load cards json into dictionary for runtime use
+  try:
+    f = open("json/cards.json")
     cardsdict = json.load(f)
     f.close
   except:
-    print ("default cards not found please git pull to download them")
+    try:
+      print("custom cards not found, attempting to load default-cards")
+      f = open("json/default-cards.json")
+      cardsdict = json.load(f)
+      f.close
+    except:
+      print ("default cards not found please git pull to download them")
     
-# load messages json into dictionary for runtime use
-try:
-  f = open("json/messages.json")
-  messagesdict = json.load(f)
-  f.close()
-except:
+  # load messages json into dictionary for runtime use
   try:
-    print("custom messages not found, attempting to load default-messages")
-    f = open("json/default-messages.json")
-    cardsdict = json.load(f)
-    f.close
+    f = open("json/messages.json")
+    messagesdict = json.load(f)
+    f.close()
   except:
-    print ("default messages not found please git pull to download them")
+    try:
+      print("custom messages not found, attempting to load default-messages")
+      f = open("json/default-messages.json")
+      cardsdict = json.load(f)
+      f.close
+    except:
+      print ("default messages not found please git pull to download them")
     
   return cardsdict, messagesdict

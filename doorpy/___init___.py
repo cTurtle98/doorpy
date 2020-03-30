0cc0d2ad3a54e1adb725160e5d4a2eba37da5c09
Ciaran Farley <ciaran@cturtle98.com>
2020-03-30 01:37:30 -0700

    Update ___init___.py

--- doorpy/___init___.py
+++ doorpy/___init___.py
@@ -23,7 +23,6 @@ init file to setup flask enviroment and global variables
 """
 
 from flask import Flask, render_template, request, redirect
-import json
 import os
 import subprocess
 
@@ -32,148 +31,21 @@ configsdict = {}
 cardsdict = {}
 messagesdict = {}
 
-def load_json_to_dict():\
-    # load configs json into dictionary for runtime use
-    try:
-        f = open("configs.json")
-        configsdict = json.load(f)
-        f.close
-    except:
-        print("ERROR! Could not load configs dictionary!")
-    # load cards json into dictionary for runtime use
-    try:
-        f = open("json/cards.json")
-        cardsdict = json.load(f)
-        f.close
-    except:
-        try:
-            print("custom cards not found, attempting to load default-cards")
-            f = open("json/default-cards.json")
-            cardsdict = json.load(f)
-            f.close
-        except:
-            print ("default cards not found please git pull to download them")
-    
-    # load messages json into dictionary for runtime use
-    try:
-        f = open("json/messages.json")
-        messagesdict = json.load(f)
-        f.close()
-    except:
-        try:
-            print("custom messages not found, attempting to load default-messages")
-            f = open("json/default-messages.json")
-            cardsdict = json.load(f)
-            f.close
-        except:
-            print ("default messages not found please git pull to download them")
-    
-    return cardsdict, messagesdict
+# import json dictionary loader
+from doorpy import load_json_to_dict
 
+#load dictionaries
 cardsdict, messagesdict = load_json_to_dict()
         
 # setup the flask enviroment
 app = Flask(__name__, template_folder='templates/')
 
-@app.route('/')
-def index() :
-    '''this is the main route that the kiosk pi will be looking at'''
-    
-    #retrieve cardnum from html form
-    cardnum = request.args.get('card')
-    
-    #if you havent scanned a card yet display a page telling you to
-    if cardnum == None:
-        cardnum = "default"
-#        return render_template(messagesdict["default"]["template"], **messagesdict["default"])
-    
-    #process scanning a card and retrieving its message
-    try:
-        if cardsdict[cardnum]['active'] == 'A':
-            cardsdict[cardnum]['active'] = 'B'
-            return render_template(messagesdict[cardsdict[cardnum]['B']]['template'], **messagesdict[cardsdict[cardnum]['B']])    
-        elif cardsdict[cardnum]['active'] == 'B':
-            cardsdict[cardnum]['active'] = 'A'
-            return render_template(messagesdict[cardsdict[cardnum]['A']]['template'], **messagesdict[cardsdict[cardnum]['A']])
-    except:
-        cardsdict[cardnum] = {"A":None, "B":None, "active":"A"}
-        return render_template(messagesdict["cardnotfound"]["template"], **messagesdict["cardnotfound"])
-    
-@app.route('/edit', methods=['GET'])
-def edit() :
-    ''' remote edit page for messages and cards'''    
-    
-    templateList = os.listdir('templates/')
-    templateList.remove('pages')
-    
-    return render_template(
-        'pages/edit.jinja2',
-        cardsdict=cardsdict,
-        messagesdict=messagesdict,
-        templateList=templateList,
-    )
+from doorpy import door
+from doorpy import edit
 
-@app.route('/editcard', methods=['POST'])
-def editcard() :
-    ''' takes post request with form values from /edit and saves them to the dict then pushes them to the json files'''
-    
-    cardnum = request.form.get('cardnum')
-    
-    if cardsdict.get(cardnum) == None:
-        cardsdict[request.form.get('cardnum')] = {
-          "nickname" : request.form.get('nickname'),
-          "A" :  request.form.get('message_a'),
-          "B" :  request.form.get('message_b'),
-            "active" : "A"
-        }
-    else:
-        if request.form.get('nickname') != "":
-            cardsdict[cardnum]["nickname"] = request.form.get('nickname')
-        
-        if request.form.get('message_a') != "":
-            cardsdict[cardnum]["A"] = request.form.get('message_a')
-    
-        if request.form.get('message_b') != "":
-            cardsdict[cardnum]["B"] = request.form.get('message_b')
-    
-        cardsdict[cardnum]["active"] = "B"
-    
-    with open('json/cards.json', 'w') as f:
-        json.dump(cardsdict, f, indent=4)
-    
-    return redirect("/edit")
+from doorpy import editcard
 
-@app.route('/editmessage', methods=['POST'])
-def editmessage() :
-    ''' takes post request with form values from /edit and saves them to the dict then pushes them to the json files'''
-    
-    messagename = request.form.get('messageName')
-    
-    if messagesdict.get(messagename) == None:
-        messagesdict[request.form.get('messageName')] = {
-            "template" : request.form.get('messageTemplate'),
-            "messageType" : request.form.get('messageType'),
-            "messageSubject" : request.form.get('messageSubject'),
-            "messageStatus" : request.form.get('messageStatus')
-        }
-    else:
-        if request.form.get('messageTemplate') != "":
-            messagesdict[messagename]["template"] = request.form.get('messageTemplate')
-    
-        if request.form.get('messageType') != "":
-            messagesdict[messagename]["messageType"] = request.form.get('messageType')
-    
-        if request.form.get('messageSubject') != "":
-            messagesdict[messagename]["messageSubject"] = request.form.get('messageSubject')
-    
-        if request.form.get('messageStatus') != "":
-            messagesdict[messagename]["messageStatus"] = request.form.get('messageStatus')
-    
-    
-    with open('json/messages.json', 'w') as f:
-        json.dump(messagesdict, f, indent=4)
-    
-    return redirect("/edit")
+from doorpy import editmessage
 
 """
 @app.route('/gitpush', methods=['POST'])

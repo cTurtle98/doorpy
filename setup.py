2784ab21f0824699b30e333d3a3f400c2bc1518c
Ciaran Farley <ciaran@cturtle98.com>
2020-03-29 19:44:31 -0700

    pypa setup file
    update docstring to include license

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 """
+doorpy - raspberry pi message board for your door
+Copyright (C) 2020  Ciaran Farley (ciaran@cturtle98.com)
 
-webpy module setup.py
-By Ciaran Farley
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+--------
+
+setup.py - pypa setup file
 
 """
 
@@ -16,17 +32,54 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
   name='doorpy',
+  
+  # VERSIONING SCHEMA
+  # X.0.0 Major Release breaks compatability
+  # 0.X.0 Minor release continues compatibility
+  # 0.0.X Bug fix releases
+  # 0.0.0.devX developement releases
+  # 0.0.0.rcX release candidates
   version='1.0.0.dev1',
+  
+  # short description of project
   description='door mounted message board',
+  
+  # get long description from readme.md
   long_description=long_description,
   long_description_content_type='text/markdown',
+  
+  # url for the project source code
   url='https://github.com/cTurtle98/doorpy',
+  
+  # project author details
   author='Ciaran Farley'
   author_email='ciaran@cturtle98.com'
   
   classifiers=[
+    
+    # project maturity
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
     'Developement Status :: 3 - Alpha',
-    'Intended Audience :: DIYers',
-    'Topic :: DIY :: digital-signage',
     
+    # project is made for use by
+    'Intended Audience :: Other Audience',
     
+    # project is written in english
+    'Natural Language :: English',
+    
+    # this is the closest topic that makes sense
+    'Topic :: Utilities',
+    
+    # gplv2 license
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    
+    # python 3.6 and up
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ].
+  install_requires=['Flask'],
+)
+
